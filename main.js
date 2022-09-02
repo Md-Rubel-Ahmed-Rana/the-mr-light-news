67ac601045734be2e543f552aeb6329e7973d1d9
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-03 01:26:59 +0600

    Route or search functionality added

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ const dispalyCategory = async () => {
     // get the Category container
     const categoryContainerUl = document.getElementById("category-container");
     allCategories.forEach(category => {
-        console.log(category);
         const categoryItem = document.createElement("li");
         categoryItem.classList.add("text-center");
         categoryItem.innerHTML = `<a onclick="loadMatchedNews('${category.category_id}')" href="#" class="text-decoration-none">${category.category_name}</a>`;
@@ -21,32 +20,26 @@ const dispalyCategory = async () => {
     });
 };
 
+
+
 // load matched news
 const loadMatchedNews = async(id) => {
     const url = await `https://openapi.programming-hero.com/api/news/category/${id}`;
     const res = await fetch(url);
     const data =await res.json();
-
+    displayMatchedNews(data.data)
 }
 
-// loading all the news data
-const loadAllNews = async () => {
-    const url = await `https://openapi.programming-hero.com/api/news/category/08`;
-    const res = await fetch(url);
-    const data = await res.json();
-    return data.data;
-}
-
-
-// Display all the news to the UI
-const dispalyNews = async () => {
-    // get the news container;
+// Display the Matched news with the category
+const displayMatchedNews = (allMatchedNews) => {
+    
+    console.log(allMatchedNews.length);
     const newsContainer = document.getElementById("news-container");
-    const allNews = await loadAllNews();
-    // console.log(allNews);
-    allNews.forEach(news => {
-        // console.log(news);
-        // create element to show news to the UI
+    newsContainer.textContent = "";
+    if (allMatchedNews.length === 0) {
+        newsContainer.innerHTML = `<h3 class="text-center m-auto">No Data found</h3>`
+    }
+    allMatchedNews.forEach(news => {
         const newsDiv = document.createElement("div");
         newsDiv.classList.add("col");
         newsDiv.innerHTML = `
@@ -57,6 +50,64 @@ const dispalyNews = async () => {
                         <p class="card-text"> ${news.details.slice(0, 200)}...</p>
                     <div class="m-auto text-center">
                         <button class="btn btn-primary">Details</button>
+                    </div>
+                    <div class="d-flex gap-2 mt-3">
+                        <div>
+                            <img src="${news.author.img}" class="rounded-circle" style="height: 50px; width: 50px" />
+                        </div>
+                        <div>
+                            <p class="m-0"> ${news.author.name === null || news.author.name === "system" ? "Author Name not available" : news.author.name} </p>
+                            <p> ${news.author.published_date === null ? "No Date available" : news.author.published_date} </p>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <div class="d-flex align-items-center gap-2">
+                          <p><i class="fa-solid fa-eye"></i></p>
+                          <p> ${news.total_view}M </p>
+                        </div>
+                        <div class="m-0">
+                           <i class="fa-regular fa-star-half-stroke"></i>
+                           <i class="fa-regular fa-star"></i>
+                           <i class="fa-regular fa-star"></i>
+                           <i class="fa-regular fa-star"></i>
+                           <i class="fa-regular fa-star"></i>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        newsContainer.appendChild(newsDiv)
+    })
+}
+
+// loading all the news data
+const loadAllNews = async () => {
+    const url = await `https://openapi.programming-hero.com/api/news/category/08`;
+    const res = await fetch(url);
+    const data = await res.json();
+    dispalyNews(data.data);
+}
+
+
+
+// Display all the news to the UI
+const dispalyNews =  (data) => {
+    // console.log(data);
+    // get the news container;
+    const newsContainer = document.getElementById("news-container");
+    data.forEach(news => {
+        // create element to show news to the UI
+        const newsDiv = document.createElement("div");
+        newsDiv.classList.add("col");
+        newsDiv.innerHTML = `
+            <div class="card h-100 p-4">
+                <img src="${news.thumbnail_url}" class="card-img-top" alt="..." style="height: 300px;" >
+                <div class="card-body p-2">
+                        <h5 class="card-title"> ${news.title} </h5>
+                        <p class="card-text"> ${news.details.slice(0, 200)}...</p>
+                    <div class="m-auto text-center">
+                        <button onclick="loadDetails('${news._id}')" class="btn btn-primary">Details</button>
                     </div>
                     <div class="d-flex gap-2 mt-3">
                         <div>
@@ -89,5 +140,6 @@ const dispalyNews = async () => {
 }
 
 
-dispalyNews()
+
+loadAllNews();
 dispalyCategory();
