e9771dee012e83c5126d8922f834296197bf43e2
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-02 21:18:04 +0600

    Displaying categories to UI completed

--- main.js
+++ main.js
@@ -3,7 +3,18 @@ const loadCategories = async() => {
     const url = await `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data.news_category);
+    dispalyCategory(data.data.news_category);
+}
+
+// Display all the category to the UI
+const dispalyCategory = async (categories) => {
+    // get the Category container
+    const categoryContainerUl = document.getElementById("category-container");
+    categories.forEach(category => {
+        const categoryItem = document.createElement("li");
+        categoryItem.innerHTML = `<a href="#" class="text-decoration-none">${category.category_name}</a>`;
+        categoryContainerUl.appendChild(categoryItem)
+    });
 }
 
 loadCategories();
